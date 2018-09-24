40f29ca47daf393a063a59f121c176332040dc3f
Eddie Webb <ollitech@gmail.com>
2018-09-24 08:47:54 -0400

    usin https for checkout

---
+++ .circleci/checkout.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -e
+
+# Workaround old docker images with incorrect $HOME
+# check https://github.com/docker/docker/issues/2968 for details
+if [ "${HOME}" = "/" ]
+then
+  export HOME=$(getent passwd $(id -un) | cut -d: -f6)
+fi
+
+echo "Repository URL $CIRCLE_REPOSITORY_URL"
+
+if [ -e "${CIRCLE_WORKING_DIRECTORY}/.git" ]
+then
+  cd ${CIRCLE_WORKING_DIRECTORY}
+  git remote set-url origin "$CIRCLE_REPOSITORY_URL" || true
+else
+  mkdir -p ${CIRCLE_WORKING_DIRECTORY}
+  cd ${CIRCLE_WORKING_DIRECTORY}
+  git clone "$CIRCLE_REPOSITORY_URL" .
+fi
+
+
+
+
+
+
+
+if [ -n "$CIRCLE_TAG" ]
+then
+  git fetch --force origin "refs/tags/${CIRCLE_TAG}"
+else
+  git fetch --force origin "master:remotes/origin/master"
+fi
+
+
+if [ -n "$CIRCLE_TAG" ]
+then
+  git reset --hard "$CIRCLE_SHA1"
+  git checkout -q "$CIRCLE_TAG"
+elif [ -n "$CIRCLE_BRANCH" ]
+then
+  git reset --hard "$CIRCLE_SHA1"
+  git checkout -q -B "$CIRCLE_BRANCH"
+fi
+
+git reset --hard "$CIRCLE_SHA1"
